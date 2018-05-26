be52f935bbca74261290aa49b5a2a1cf6267ef57
Faissal Bensefia <faissaloo@gmail.com>
2018-05-26 17:07:01 +0100

    Added thread display testing

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -7,5 +7,21 @@ RSpec.describe IndexController, type: :controller do
 			get :index
 			expect(response.body).to match("1b")
 		end
+		
+		it "shows threads" do
+			script = ["A lot of loyalty for a hired gun!!",
+				"Perhaps he's wondering why someone would shoot a man before throwing him out of a plane.",
+				"At least you can talk. Who are you?",
+				"It doesn't matter who we are, what matters is our plan. No one cared who I was until I put on the mask.",
+				"If I pull that off would you die.",
+				"It would be extreamly painful....",
+				"You're a big guy!",
+				"For you!"]
+				
+			script.each {|i|
+				Post.new(message: i)
+				expect(response.body).to match(i)
+			}
+		end
 	end
 end
