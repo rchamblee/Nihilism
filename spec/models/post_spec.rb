d2cb550887713f20293138b206b2908063c68374
Faissal Bensefia <faissaloo@gmail.com>
2018-05-26 16:45:27 +0100

    Added post message validation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   it "Is invalid without text" do
     post = Post.new(message: nil)
     expect(post).to_not be_valid
