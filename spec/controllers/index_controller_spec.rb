2e535fefe3bbd4fc88ad976757f4a86eef0ea831
Faissal Bensefia <faissaloo@gmail.com>
2018-05-26 19:24:17 +0100

    Added test for posting via form to board

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe IndexController, type: :controller do
-	render_views
-	script = ["A lot of loyalty for a hired gun!!",
-		"Perhaps he's wondering why someone would shoot a man before throwing him out of a plane.",
-		"At least you can talk. Who are you?",
-		"It doesn't matter who we are, what matters is our plan. No one cared who I was until I put on the mask.",
-		"If I pull that off would you die.",
-		"It would be extreamly painful....",
-		"You're a big guy!",
-		"For you!"]
+script = ["A lot of loyalty for a hired gun!!",
+	"Perhaps he's wondering why someone would shoot a man before throwing him out of a plane.",
+	"At least you can talk. Who are you?",
+	"It doesn't matter who we are, what matters is our plan. No one cared who I was until I put on the mask.",
+	"If I pull that off would you die.",
+	"It would be extreamly painful....",
+	"You're a big guy!",
+	"For you!"]
 				
 
+RSpec.describe IndexController, type: :controller do
+	render_views
 	describe "index" do
 		it "has a title" do
 			get :index
@@ -34,5 +34,15 @@ RSpec.describe IndexController, type: :controller do
 				expect(response.body).to include(CGI.escape_html(i))
 			}
 		end
+	end
+end
+
+feature 'Forms' do
+	scenario "posting to board" do
+		visit "/"
+		fill_in "msg", with: script[0]
+		click_button "post"
+		visit "/"
+		expect(page).to have_content(script[0])
 	end
 end
