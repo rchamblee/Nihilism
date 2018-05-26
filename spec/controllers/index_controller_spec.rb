02dc9b3b82856e508b036921003d902148752e94
Faissal Bensefia <faissaloo@gmail.com>
2018-05-26 18:46:58 +0100

    Added test for threading

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe IndexController, type: :controller do
 	render_views
+	script = ["A lot of loyalty for a hired gun!!",
+		"Perhaps he's wondering why someone would shoot a man before throwing him out of a plane.",
+		"At least you can talk. Who are you?",
+		"It doesn't matter who we are, what matters is our plan. No one cared who I was until I put on the mask.",
+		"If I pull that off would you die.",
+		"It would be extreamly painful....",
+		"You're a big guy!",
+		"For you!"]
+				
+
 	describe "index" do
 		it "has a title" do
 			get :index
@@ -9,20 +19,20 @@ RSpec.describe IndexController, type: :controller do
 		end
 		
 		it "shows threads" do
-			script = ["A lot of loyalty for a hired gun!!",
-				"Perhaps he's wondering why someone would shoot a man before throwing him out of a plane.",
-				"At least you can talk. Who are you?",
-				"It doesn't matter who we are, what matters is our plan. No one cared who I was until I put on the mask.",
-				"If I pull that off would you die.",
-				"It would be extreamly painful....",
-				"You're a big guy!",
-				"For you!"]
-				
 			script.each {|i|
 				Post.create(message: i)
 				get :index
 				expect(response.body).to include(CGI.escape_html(i))
 			}
 		end
+		
+		it "shows children" do
+			last_post = nil
+			script.each {|i|
+				last_post = Post.create(parent: last_post&.id, message: i)
+				get :index
+				expect(response.body).to include(CGI.escape_html(i))
+			}
+		end		
 	end
 end
