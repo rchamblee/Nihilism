00bff9cbe0a1af7c09ff8feb84855a7e3220f3f3
Faissal Bensefia <faissaloo@gmail.com>
2018-05-27 03:41:07 +0100

    posts are now deleted after 255 posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
 	validates :message, :presence => true
+	after_create :post_cap
+	
+	def post_cap
+		if Post.count>=255
+			Post.order('created_at asc').first.destroy
+		end
+	end
+	#Need to change posts who have this post as its parent to have the parent nil when destroyed
 end
