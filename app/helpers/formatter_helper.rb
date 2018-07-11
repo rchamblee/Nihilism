1a38d2e0df458ecced07deeb9f548f268507fa59
Faissal Bensefia <faissaloo@gmail.com>
2018-07-11 21:18:37 +0100

    Refactoring, formatting and various tweaks

---
+++ app/helpers/formatter_helper.rb
@@ -0,0 +1,30 @@
+module FormatterHelper
+	def self.escape(str)
+		str.gsub(/[&><"'#;]/,"&" => "&amp;", ">" => "&gt;", "<" => "&lt;", '"' => "&quot;", "'" => "&#39;", "#" => "&#x23;", ";" => "&#x3B;")
+	end
+
+	@@spec = Riseup::Spec.new([[escape("\n"),"<br/>"],
+		[escape("\\="),escape("=")],
+		[escape("\\$"),escape("$")],
+		[escape("\\`"),escape("`")],
+		[escape("\\*"),escape("*")],
+		[escape("\\#"),escape("#")],
+		[escape("=="),"<span class=\"redtext\">","</span>"],
+		[escape("$"),"<span class=\"rainbowtext\">","</span>"],
+		[escape("#"),"<span class=\"threedtext\">","</span>"],
+		[escape("`"), "<code>","</code>"],
+		[escape("**"),"<b>","</b>"],
+		[escape("*"),"<i>","</i>"],
+		])
+	
+	def self.format(str)
+		Riseup::Parser.new(self.escape(str),@@spec).parse
+	end
+
+	@@formatted_headings = Rails.configuration.headings.map {|str| format(str)}
+
+	#Picks a random heading out to be displayed
+	def self.heading
+		@@formatted_headings.sample
+	end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,30 +1,10 @@
 class Post < ActiveRecord::Base
-	#Move this later
-	#The built in encoder is inadequet as it doesn't do '#' and ';'
-	def self.escape(str)
-		str.gsub(/[&><"'#;]/,"&" => "&amp;", ">" => "&gt;", "<" => "&lt;", '"' => "&quot;", "'" => "&#39;", "#" => "&#x23;", ";" => "&#x3B;")
-	end
-	
-	@@spec = Riseup::Spec.new([[escape("\n"),"<br/>"],
-		[escape("\\="),escape("=")],
-		[escape("\\$"),escape("$")],
-		[escape("\\`"),escape("`")],
-		[escape("\\*"),escape("*")],
-		[escape("\\#"),escape("#")],
-		[escape("=="),"<span class=\"redtext\">","</span>"],
-		[escape("$"),"<span class=\"rainbowtext\">","</span>"],
-		[escape("#"),"<span class=\"threedtext\">","</span>"],
-		[escape("`"), "<code>","</code>"],
-		[escape("**"),"<b>","</b>"],
-		[escape("*"),"<i>","</i>"],
-		])
-
 	validates :message, :presence => true
 	after_create :post_cap
 	after_update :update_children
 
 	def html
-		"<p>"+Riseup::Parser.new(self.class.escape(self.message),@@spec).parse+"</p>"
+		FormatterHelper.format(self.message)
 	end
 	
 	def self.reply(msg, parent_id="")
