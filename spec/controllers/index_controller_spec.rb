c03ed2fea278a8da10ef34dd0bff92cb8b4e5d4b
faissaloo <faissaloo@gmail.com>
2018-09-01 13:08:11 +0100

    Rename to Ratwires

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -8,16 +8,11 @@ script = ["A lot of loyalty for a hired gun!!",
 	"It would be extreamly painful....",
 	"You're a big guy!",
 	"For you!"]
-				
+
 
 RSpec.describe IndexController, type: :controller do
 	render_views
 	describe "index" do
-		it "has a title" do
-			get :index
-			expect(response.body).to match("1b")
-		end
-		
 		it "shows threads" do
 			script.each {|i|
 				Post.create(message: i)
@@ -25,7 +20,7 @@ RSpec.describe IndexController, type: :controller do
 				expect(response.body).to include(CGI.escape_html(i))
 			}
 		end
-		
+
 		it "shows children" do
 			last_post = nil
 			script.each {|i|
@@ -34,13 +29,13 @@ RSpec.describe IndexController, type: :controller do
 				expect(response.body).to include(CGI.escape_html(i))
 			}
 		end
-		
+
 		it "shows post timestamps" do
 			Post.create(message: script[0])
 			get :index
 			expect(response.body).to have_text(/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9] [0-9][0-9]:[0-9][0-9]/)
 		end
-		
+
 		it "posts are shown in the correct order" do
 			post_id = Post.reply("A")
 			Post.reply("B", post_id)
@@ -49,7 +44,7 @@ RSpec.describe IndexController, type: :controller do
 			get :index
 			expect(response.body).to have_text(/D.*A.*C.*B/m)
 		end
-		
+
 	end
 end
 
@@ -57,14 +52,14 @@ feature 'Forms' do
 	scenario "basic formating" do
 		Post.reply("I ==ponder== *of* **something** $great$\nMy $$lungs$$ will fill and \#then\# `deflate`")
 		visit "/"
-		expect(page).to have_xpath("//div[@class='post_content']/p/br")	
+		expect(page).to have_xpath("//div[@class='post_content']/p/br")
 		expect(page).to have_xpath("//div[@class='post_content']/p/span[@class='redtext']")
 		expect(page).to have_xpath("//div[@class='post_content']/p/span[@class='rainbowtext']")
 		expect(page).to have_xpath("//div[@class='post_content']/p/span[@class='threedtext']")
 		expect(page).to have_xpath("//div[@class='post_content']/p/span[@class='shaketext']")
 		expect(page).to have_xpath("//div[@class='post_content']/p/code")
-		expect(page).to have_xpath("//div[@class='post_content']/p/i")	
-		expect(page).to have_xpath("//div[@class='post_content']/p/b")	
+		expect(page).to have_xpath("//div[@class='post_content']/p/i")
+		expect(page).to have_xpath("//div[@class='post_content']/p/b")
 	end
 
 	scenario "formating escapes" do
@@ -75,14 +70,14 @@ feature 'Forms' do
 		expect(page).to_not have_xpath("//div[@class='post_content']/p/span[@class='threedtext']")
 		expect(page).to_not have_xpath("//div[@class='post_content']/p/span[@class='shaketext']")
 		expect(page).to_not have_xpath("//div[@class='post_content']/p/code")
-		expect(page).to_not have_xpath("//div[@class='post_content']/p/i")	
-		expect(page).to_not have_xpath("//div[@class='post_content']/p/b")	
+		expect(page).to_not have_xpath("//div[@class='post_content']/p/i")
+		expect(page).to_not have_xpath("//div[@class='post_content']/p/b")
 	end
-	
+
 	scenario "should not be able to add images" do
 		Post.reply('![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")')
 		visit "/"
-		expect(page).to_not have_xpath("//div[@class='post_content']/p/img")		
+		expect(page).to_not have_xpath("//div[@class='post_content']/p/img")
 	end
 
 	scenario "posting to board" do
@@ -92,7 +87,7 @@ feature 'Forms' do
 		visit "/"
 		expect(page).to have_content(script[0])
 	end
-	
+
 	scenario "posting reply" do
 		visit "/"
 		fill_in "msg", with: script[0]
@@ -102,14 +97,14 @@ feature 'Forms' do
 		expect(page).to have_xpath("//div/div")
 		expect(page).to have_content(script[1])
 	end
-	
+
 	scenario "posts have reply links" do
 		post_id = Post.create(message: script[0]).id.to_s
 		visit "/"
 		click_link("reply-" + post_id)
 		expect(current_path).to eq("/" + post_id)
 	end
-	
+
 	scenario "post number being replied to is shown" do
 		post_id = Post.create(message: script[0]).id.to_s
 		visit "/"+post_id
@@ -123,13 +118,13 @@ feature 'Forms' do
 			expect(page).to have_content(post_id)
 		}
 	end
-	
+
 	scenario "empty posts cause a soft error" do
 		visit "/"
 		click_button "post"
 		expect(page.status_code).to eq(400)
 	end
-	
+
 	scenario "attempting to reply to a nonexistent post should" do
 		visit "/57"
 		fill_in "msg", with: script[0]
