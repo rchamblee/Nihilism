6624652b54f5e8125c18fbfe04db7b7e06691be0
faissaloo <faissaloo@gmail.com>
2018-09-07 18:47:04 +0100

    Post trimming system now orphans posts and deletes the correct stuff

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,20 @@
 class Post < ActiveRecord::Base
 	validates :message, :presence => true
-	after_create :post_cap
 
 	def html
 		FormatterHelper.format(self.message)
 	end
 
+	#Makes a post then removes the oldest unreplied to post, orphaning its children
 	def self.reply(msg, parent_id=nil)
-		post_id = Post.create(message: msg, parent: parent_id).id
+		post_to_delete = nil
+		if Post.count >= 254
+			post_to_delete = Post.order('updated_at asc').first
+		end
+
+		post = Post.create(message: msg, parent: parent_id)
+		post_id = post.id
+
 		if parent_id != "" && !parent_id.nil?
 			last_reply = post_id
 			until parent_id.nil?
@@ -17,18 +24,19 @@ class Post < ActiveRecord::Base
 				last_reply = parent
 				parent_id = last_reply.parent
 			end
+		end
 
+		logger.error "Attempting fresh trim"
+		unless post_to_delete.nil?
+			#Doesn't modify updated_at, makes all children top level posts
+			posts_to_orphan = Post.where(parent: post_to_delete.id)
+			posts_to_orphan.update_all(parent: nil)
+			post_to_delete.destroy
 		end
 		post_id
 	end
 
 	def self.get_replies(parent_id="")
 		Post.where(parent: parent_id).order("updated_at desc")
-	end
-
-	def post_cap
-		if Post.count>=255
-			Post.order('created_at asc').first.destroy
-		end
 	end
 end
